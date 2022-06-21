137909fd0e99d5330e871f770d172db7d99213fe
Asti <astistateofmind@gmail.com>
2022-06-21 09:03:41 -0700

    fix mistake

--- controllers/flights.js
+++ controllers/flights.js
@@ -12,12 +12,13 @@ function newFlight(req, res) {
 
 function create(req, res) {
     req.body.flightNo = req.body.flightNo.trim();
+    const flight = new Flight(req.body);
     res.redirect('/flights');
 }
 
 function index(req, res) {
     Flight.find({}, function (err, flights) {
-        //console.log(movies)
+        //console.log(flights)
         res.render('flights/index', {
             flights
         })
