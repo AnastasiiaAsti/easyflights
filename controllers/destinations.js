1209e4daef04eba24492b8deda08fb2d11034f5d
Asti <astistateofmind@gmail.com>
2022-06-21 15:58:07 -0700

    add UI for show.ejs

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -7,7 +7,7 @@ module.exports = {
 function create(req, res) {
     Flight.findById(req.params.id, function (err, flight) {
         //push subdoc into Mongoose arrays
-        flight.reviews.push(req.body)
+        flight.destinations.push(req.body)
         //save any changes to the parent doc
         flight.save(function (err) {
             //respond to the request, in this case redirect
